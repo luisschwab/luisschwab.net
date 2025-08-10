14a21e6e3f66a534bf494c61a9512e76e12ee12c
Luis Schwab <luisschwab@protonmail.com>
2025-08-09 23:26:16 -0400

    chore(quotes): nits

--- src/quotes.rs
+++ src/quotes.rs
@@ -1,6 +1,7 @@
 pub(crate) const QUOTES: &[(&str, &str)] = &[
     (
-        "If you want to build a ship, don't drum up the men to gather wood, divide the work, and give orders. Instead, teach them to yearn for the vast and endless sea.",
+        "If you want to build a ship, don't drum up the men to gather wood, divide the work,
+        and give orders. Instead, teach them to yearn for the vast and endless sea.",
         "Antoine de Saint-Exupéry",
     ),
     (
@@ -16,19 +17,27 @@ pub(crate) const QUOTES: &[(&str, &str)] = &[
         "Edgar Allan Poe",
     ),
     (
-        "Cypherpunks write code. We know that someone has to write software to defend privacy, and since we can't get privacy unless we all do, we're going to write it. We publish our code so that our fellow Cypherpunks may practice and play with it. Our code is free for all to use, worldwide.",
+        "Cypherpunks write code. We know that someone has to write software to defend privacy,
+        and since we can't get privacy unless we all do, we're going to write it. We publish
+        our code so that our fellow Cypherpunks may practice and play with it. Our code is free
+        for all to use, worldwide.",
         "Eric Hughes",
     ),
     (
-        "Once men turned their thinking over to machines in the hope that this would set them free. But that only permitted other men with machines to enslave them.",
+        "Once men turned their thinking over to machines in the hope that this would set them
+        free. But that only permitted other men with machines to enslave them.",
         "Frank Herbert",
     ),
     (
-        "When plunder is abetted by the law, it does not fear your courts, your gendarmes, and your prisons. Rather, it may call upon them for help.",
+        "When plunder is abetted by the law, it does not fear your courts,
+        your gendarmes, and your prisons. Rather, it may call upon them for help.",
         "Frederic Bastiat",
     ),
     (
-        "I don't believe we shall ever have a good money again before we take the thing out of the hands of government, that is, we can't take it violently out of the hands of government, all we can do is by some sly roundabout way introduce something that they can't stop.",
+        "I don't believe we shall ever have a good money again before we take the
+        thing out of the hands of government, that is, we can't take it violently
+        out of the hands of government, all we can do is by some sly roundabout
+        way introduce something that they can't stop.",
         "Friedrich Hayek",
     ),
     (
@@ -44,15 +53,17 @@ pub(crate) const QUOTES: &[(&str, &str)] = &[
         "Fyodor Dostoyevsky",
     ),
     (
-        "So long as man remains free he strives for nothing so incessantly and so painfully as to find someone to worship.",
+        "So long as man remains free he strives for nothing so incessantly and so
+        painfully as to find someone to worship.",
         "Fyodor Dostoyevsky",
     ),
     (
-        "There is no subject so old that nothing new can be said about it. ",
+        "There is no subject so old that nothing new can be said about it.",
         "Fyodor Dostoyevsky",
     ),
     (
-        "Nothing is more seductive to a man than his freedom of conscience, but nothing is a greater cause of suffering.",
+        "Nothing is more seductive to a man than his freedom of conscience,
+        but nothing is a greater cause of suffering.",
         "Fyodor Dostoyevsky",
     ),
     (
@@ -60,23 +71,30 @@ pub(crate) const QUOTES: &[(&str, &str)] = &[
         "Henry David Thoreau",
     ),
     (
-        "The cost of a thing is the amount of what I will call life which is required to be exchanged for it, immediately or in the long run.",
+        "The cost of a thing is the amount of what I will call life
+        which is required to be exchanged for it, immediately or in the long run.",
         "Henry David Thoreau",
     ),
     (
-        "A man is rich in proportion to the number of things which he can afford to let alone.",
+        "A man is rich in proportion to the number of things
+        which he can afford to let alone.",
         "Henry David Thoreau",
     ),
     (
-        "A truly good book…teaches me better than to read it. I must soon lay it down and commence living on its hint.",
+        "A truly good book…teaches me better than to read it.
+        I must soon lay it down and commence living on its hint.",
         "Henry David Thoreau",
     ),
     (
-        "I went to the woods because I wished to live deliberately, to front only the essential facts of life, and see if I could not learn what it had to teach, and not, when I came to die, discover that I had not lived.",
+        "I went to the woods because I wished to live deliberately,
+        to front only the essential facts of life, and see if I could
+        not learn what it had to teach, and not, when I came to die,
+        discover that I had not lived.",
         "Henry David Thoreau",
     ),
     (
-        "I know of no more encouraging fact than the unquestionable ability of man to elevate his life by conscious endeavour.",
+        "I know of no more encouraging fact than the unquestionable
+        ability of man to elevate his life by conscious endeavour.",
         "Henry David Thoreau",
     ),
     (
@@ -88,7 +106,8 @@ pub(crate) const QUOTES: &[(&str, &str)] = &[
         "Isaac Asimov",
     ),
     (
-        "If I have been able to see further, it was only because I stood on the shoulders of giants.",
+        "If I have been able to see further, it was only
+        because I stood on the shoulders of giants.",
         "Isaac Newton",
     ),
     (
@@ -96,7 +115,8 @@ pub(crate) const QUOTES: &[(&str, &str)] = &[
         "Louis Brandeis",
     ),
     (
-        "It ain't what you don't know that gets you into trouble. It's what you know for sure that just ain't so.",
+        "It ain't what you don't know that gets you into trouble.
+        It's what you know for sure that just ain't so.",
         "Mark Twain",
     ),
     (
@@ -105,35 +125,42 @@ pub(crate) const QUOTES: &[(&str, &str)] = &[
     ),
     ("I dreamt of wordly success once.", "Miyamoto Musashi"),
     (
-        "Unfortunately, no one can be told what the Matrix is. You have to see it for yourself.",
+        "Unfortunately, no one can be told what the Matrix is.
+        You have to see it for yourself.",
         "Morpheus",
     ),
     (
-        "There are only two tragedies in life: one is not getting what you want, and the other is getting it.",
+        "There are only two tragedies in life: one is not
+        getting what you want, and the other is getting it.",
         "Oscar Wilde",
     ),
     (
-        "I cannot remember the books I've read any more than the meals I have eaten; even so, they have made me.",
+        "I cannot remember the books I've read any more than
+        the meals I have eaten; even so, they have made me.",
         "Ralph Waldo Emerson",
     ),
     (
-        "Machinery is aggressive. The weaver becomes a web, the machinist a machine. If you do not use tools, they use you.",
+        "Machinery is aggressive. The weaver becomes a web,
+        the machinist a machine. If you do not use tools, they use you.",
         "Ralph Waldo Emerson",
     ),
     (
-        "Two roads diverged in a wood, and I— I took the one less traveled by, And that has made all the difference.",
+        "Two roads diverged in a wood, and I— I took the one
+        less traveled by, And that has made all the difference.",
         "Robert Frost",
     ),
     (
-        "If you don't believe me or don't get it, I don't have time to try to convince you, sorry.",
+        "If you don't believe me or don't get it,
+        I don't have time to try to convince you, sorry.",
         "Satoshi Nakamoto",
     ),
     (
-        "We can't have full knowledge all at once. We must start by believing; then afterwards we may be led on to master the evidence for ourselves.",
+        "We can't have full knowledge all at once. We must start by believing;
+        then afterwards we may be led on to master the evidence for ourselves.",
         "Thomas Aquinas",
     ),
     (
-        "I have never gone amongst men without coming back less a man",
+        "I have never gone amongst men without coming back less a man.",
         "Thomas à Kempis",
     ),
     (
@@ -141,11 +168,13 @@ pub(crate) const QUOTES: &[(&str, &str)] = &[
         "dergigi",
     ),
     (
-        "Matters of great concern should be treated lightly.” Master Ittei commented, “Matters of small concern should be treated seriously.",
+        "Matters of great concern should be treated lightly.” Master Ittei commented,
+        “Matters of small concern should be treated seriously.",
         "Yamamoto Tsunetomo, Hagakure: The Book of the Samurai",
     ),
     (
-        "In the modern world, you are defined not by what you say no to; but what you say yes to.",
+        "In the modern world, you are defined not by
+        what you say no to; but what you say yes to.",
         "George Hotz",
     ),
     (
@@ -161,18 +190,22 @@ pub(crate) const QUOTES: &[(&str, &str)] = &[
         "Unknown Author",
     ),
     (
-        "When you start to walk on The Way, The Way appears.",
+        "When you start to walk on the way, the way appears.",
         "Rumi",
     ),
+    ("Get busy livin'... Or get busy dyin'", "Andy Dufresne"),
     (
-        "Get busy livin'... Or get busy dyin'",
-        "Andy Dufresne"
+        "Forget that...there are places in this world
+        that aren't made out of stone. That there's something
+        inside...that they can't get to, that they can't touch. That's yours.",
+        "Andy Dufresne",
     ),
     (
-        "Forget that...there are places in this world that aren't made out of stone.
-        That there's something inside...that they can't get to, that they can't touch. That's yours.",
-        "Andy Dufresne"
-
+        "There is something to be learned from a rainstorm. When meeting with a sudden shower,
+        you try not to get wet and run quickly along the road.By doing such things as passing
+        under the eaves of houses, you still get wet.When you are resolved from the beginning,
+        you will not be perplexed, though you willstill get the same soaking.",
+        "Yamamoto Tsunetomo",
     ),
     (
         "There is something to be learned from a rainstorm.
@@ -180,7 +213,7 @@ pub(crate) const QUOTES: &[(&str, &str)] = &[
         By doing such things as passing under the eaves of houses, you still get wet.
         When you are resolved from the beginning, you will not be perplexed, though you will
         still get the same soaking.",
-        "Yamamoto Tsunetomo"
+        "Yamamoto Tsunetomo",
     ),
     (
         "If privacy is outlawed, only outlaws will have privacy.",
